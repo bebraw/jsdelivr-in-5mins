ee8083e89b21cfa6c50e46b72c392a3fe2348a94
Juho Vepsalainen <bebraw@gmail.com>
2015-10-19 15:40:11 +0300

    Add sites using jsDelivr

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -10,6 +10,8 @@ import images from "./images";
 
 preloader([images.network]);
 
+const slideTransition = ['zoom', 'fade'];
+
 export default class extends React.Component {
   render() {
     return (
@@ -22,75 +24,36 @@ export default class extends React.Component {
             in 5 Minutes
           </Heading>
         </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
+        <Slide transition={slideTransition} bgColor="primary">
+          <Heading size={1} fit lineHeight={1} textColor="tertiary">
+            Multi-CDN
+          </Heading>
+        </Slide>
+        <Slide transition={slideTransition} bgColor="primary">
           <Image src={images.network} margin="0px auto 40px" height="475px"/>
         </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
+        <Slide transition={slideTransition} bgColor="primary">
+          <Heading size={1} fit lineHeight={1} textColor="tertiary">
+            Sites Using jsDelivr
+          </Heading>
+        </Slide>
+        <Slide transition={slideTransition} bgColor="primary" textColor="tertiary">
+          <List>
+            <ListItem><Appear fid="1">Urban Dictionary</Appear></ListItem>
+            <ListItem><Appear fid="2">Fontawesome</Appear></ListItem>
+            <ListItem><Appear fid="3">Windows Phone Shop (ru)</Appear></ListItem>
+            <ListItem><Appear fid="4">Algolia</Appear></ListItem>
+            <ListItem><Appear fid="5">Honda</Appear></ListItem>
+            <ListItem><Appear fid="6">Atlassian</Appear></ListItem>
+            <ListItem><Appear fid="7">Evernote</Appear></ListItem>
+            <ListItem><Appear fid="8">...</Appear></ListItem>
+          </List>
+        </Slide>
+        <Slide transition={slideTransition} bgColor="primary">
           <iframe
             width="100%"
             height="600px"
             src="https://www.jsdelivr.com/statistics"/>
-        </Slide>
-        <Slide transition={["slide"]}>
-          <Appear fid="1">
-            <Heading size={1} caps fit textColor="primary">
-              Full Width
-            </Heading>
-          </Appear>
-          <Appear fid="2">
-            <Heading size={1} caps fit textColor="tertiary">
-              Adjustable Darkness
-            </Heading>
-          </Appear>
-          <Appear fid="3">
-            <Heading size={1} caps fit textColor="primary">
-              Background Imagery
-            </Heading>
-          </Appear>
-        </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Heading caps fit>Flexible Layouts</Heading>
-          <Layout>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Left
-              </Heading>
-            </Fill>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Right
-              </Heading>
-            </Fill>
-          </Layout>
-        </Slide>
-        <Slide transition={["slide"]} bgColor="black">
-          <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={["slide", "spin"]} bgColor="primary">
-          <Heading caps fit size={1} textColor="tertiary">
-            Smooth
-          </Heading>
-          <Heading caps fit size={1} textColor="secondary">
-            Combinable Transitions
-          </Heading>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <List>
-            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
-            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
-            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
-            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
-            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
-            <ListItem><Appear fid="6">And...</Appear></ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["slide"]} bgColor="primary">
-          <Heading size={1} caps fit textColor="tertiary">
-            That's all folks
-          </Heading>
         </Slide>
       </Deck>
     );
